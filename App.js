bd11e1840d8d4db698f5915600c8a8901d7a2a47
Dep321 <91454349+Dep321@users.noreply.github.com>
2023-04-10 15:22:12 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { Component } from 'react';
-  
-    import Routes from './Route';
-  export default class App extends Component<{}> {
-      render() {
-  return (
-    <View style={styles.container}>
-            <StatusBar
-              backgroundColor="#fff"
-              barStyle="dark-content"
-            />
-            <Routes/>
-          </View>
-        );
-      }
-    }
+import React, { useState } from "react";
+import { View, TextInput, Text } from "react-native";
+import YoutubePlayer from "./MyYoutubePlayer";
+import Timer from "./Timer";
+ 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [playing, setPlaying] = useState('false');
+  const [duration, setDuration] = useState(59);
+  const playlistId = "RDGMEMYH9CUrFO7CfLJpaD7UR85wVMCg-4ZHI93WI"
+
+  const handleStateChange = status => {
+   setPlayerStatus(status);
+ };
+
+  const minutesChange = value => {
+   const minutes = parseInt(value)
+   const seconds = duration % 60
+   const newDuration = minutes * 60 + seconds;
+   setDuration (newDuration)
+}
+
+  const secondsChange = value => {
+   const minutes = Math.floor(duration / 60)
+   const seconds = parseInt(value)
+   const newDuration = minutes * 60 + seconds;
+   setDuration (newDuration)
+}
+
+  const playerRestart = () => {
+   setPlaying('playing');
+}
+
+  return (
+    <View>
+     <YoutubePlayer playlistId ={playlistId} />
+       <View style={{flex: 1, alightItems: 'center', justifyContent: 'center'}}>
+         <View style ={{flexDirection: 'row'}}>
+           <TextInput
+             style={{ height: 30, width: 30, marginBottom: 30, textAlign: 'center'}}
+             keyboardType='numeric'
+             maxLength = {2}
+             placeholder = "MM"
+             onChangeText={minutesChange}
+           />
+         <Text style={{fontSize: 24, marginBottom: 20}}>:</Text>
+           <TextInput
+             style={{ height: 30, width: 30, marginBottom: 30, textAlign: 'center'}}
+             keyboardType='numeric'
+             maxLength = {2}
+             placeholder = "SS"
+             onChangeText={secondsChange}
+           />
+          </View>
+          <Timer duration={duration} playing={playing} playerRestart={playerRestart} />     
+      </View>
+    </View>
+  );
+}
